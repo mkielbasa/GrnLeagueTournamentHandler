a0c87f2c91639137267fb2d77ba83ac51772f1e6
BuildTools <unconfigured@null.spigotmc.org>
2022-04-11 12:57:15 +0200

    Initialized Spring boot project.

---
+++ GrnTournament/src/main/java/com/example/grntournament/GrnTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.example.grntournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrnTournamentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrnTournamentApplication.class, args);
+    }
+
+}
---
+++ GrnTournament/src/test/java/com/example/grntournament/GrnTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grntournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrnTournamentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
